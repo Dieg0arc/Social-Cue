ea9682b1da3d6677f0f1d5ee3f8cfb52463b4d36
Juan Jose Garay Cuellar <jgaray2223@cue.edu.co>
2025-06-18 16:06:04 -0500

    fjieowjfiw

--- Social_Back/post-service/models/follow.go
+++ Social_Back/post-service/models/follow.go
@@ -6,6 +6,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+//owierbewoi
 type Follow struct {
 	ID         primitive.ObjectID `bson:"_id,omitempty" json:"id"`
 	SeguidorID primitive.ObjectID `bson:"seguidorId" json:"seguidorId"`
